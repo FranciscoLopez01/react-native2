cbef761b2b4a425ed282f6d9d7b5ef4855d90b10
Francisco <franciscoalbertolopez01@gmail.com>
2022-03-17 19:37:53 -0300

    ADD

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="container">
+      <header class="d-flex justify-content-center py-3">
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <a href="#" class="nav-link active" aria-current="page">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              Features
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              Pricing
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              FAQs
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              About
+            </a>
+          </li>
+        </ul>
       </header>
     </div>
   );
 }
-
 export default App;
